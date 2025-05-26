a19137ab60dfe5693e76c1281ff02bf976395a38
Thereallo <thereallo1026@gmail.com>
2025-05-26 05:18:34 -0400

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,35 @@ import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
-  title: "Skirk Countdown",
-  description: "Skirk Countdown",
+	title: "Skirk Countdown",
+	description:
+		'Yet, his "Foul Legacy" was not originally his. Rather, it was taught to him by that solitary girl who dwelt in the darkest corners of the universe.',
+	openGraph: {
+		title: "Skirk Countdown | Genshin Impact Countdown",
+		type: "website",
+		url: "https://skirk.live",
+		images: "/meta-image.png",
+		description:
+			'Yet, his "Foul Legacy" was not originally his. Rather, it was taught to him by that solitary girl who dwelt in the darkest corners of the universe.',
+	},
+	themeColor: "#715acd",
+	twitter: {
+		card: "summary_large_image",
+	},
+	icons: {
+		icon: "icon.jpg",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-			<html lang="en">
-				<link rel="icon" href="/favicon.ico" sizes="any" />
-				<body className={`${GeistSans.className} antialiased`}>{children}</body>
-			</html>
-		);
+	return (
+		<html lang="en">
+			<link rel="icon" href="/favicon.ico" sizes="any" />
+			<body className={`${GeistSans.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
